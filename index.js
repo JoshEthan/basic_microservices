c7ab47e27069dcfa240298a4e9033ebcf8718e54
Joshua Rolph <joshuaeris@gmail.com>
2024-09-16 09:47:52 -0600

    Created a basic email service.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import UserService from "./services/user.service.js";
+import EmailService from "./services/email.service.js";
 
 async function startApp() {
   // Start services
   await UserService.start();
+  await EmailService.start();
 
   try {
     // Simluate user creation
@@ -13,6 +15,13 @@ async function startApp() {
     console.log("Created user:", newUser);
     const users = await UserService.call("user.getUsers");
     console.log("Users:", users);
+    // Simluate sending email
+    const emailResult = await EmailService.call("email.sendEmail", {
+      recipient: newUser.email,
+      subject: "Welcome to our platform",
+      content: "Thank you for signing up",
+    });
+    console.log("Email result:", emailResult);
   } catch (error) {
     console.error(error);
   } finally {
